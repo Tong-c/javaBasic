30b2007d112161c220ab0ae77a19211ee4c7769c
tong <1972376180@qq.com>
2018-05-02 18:31:43 +0800

    1.page34-page44,條件控制剩餘部分，循環控制

---
+++ src/main/java/com/tong/logic/chapterOne/Three.java
@@ -0,0 +1,85 @@
+package com.tong.logic.chapterOne;
+
+import java.util.Scanner;
+
+public class Three {
+    public static void main(String[] args) {
+        testForContinue();
+    }
+
+    public static void testWhile(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("please input password");
+        int num = reader.nextInt();
+        int password = 6789;
+        while (num != password){
+            System.out.println("please input password");
+            num = reader.nextInt();
+        }
+        System.out.println("correct");
+        reader.close();
+    }
+
+    //不管條件語句是什麽，代碼至少執行一次，do/while
+    public static void testDoWhile(){
+        Scanner reader = new Scanner(System.in);
+        int password = 6789;
+        int num = 0;
+        do{
+            System.out.println("please input password");
+            num = reader.nextInt();
+        }while (num != password);
+        System.out.println("correct");
+        reader.close();
+    }
+
+    public static void testFor(){
+        int[] arr = {1,2,3,4};
+        for(int i  = 0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    //死循環
+    public static void testForTwo(){
+        for(;;){}
+    }
+
+    public static void testForEach(){
+        int[] arr = {1,2,3,4};
+        for(int element:arr){
+            System.out.println(element);
+        }
+    }
+
+    public static void testForBreak(){
+        int[] arr = {1,2,3,4};
+        int toSearch = 100;
+        int i = 0;
+        for(;i<arr.length;i++){
+            if(arr[i]==toSearch){
+                break;
+            }
+        }
+        if(i!=arr.length){
+            System.out.println("found");
+        }else {
+            System.out.println("not found");
+        }
+
+    }
+
+    public static void testForContinue(){
+        int[] arr = {1,2,3,4};
+        int toSearch = 2;
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i] != toSearch){
+                continue;
+            }
+            count++;
+        }
+
+        System.out.println("found count "+count);
+    }
+}
--- src/main/java/com/tong/logic/chapterOne/Two.java
+++ src/main/java/com/tong/logic/chapterOne/Two.java
@@ -2,7 +2,7 @@ package com.tong.logic.chapterOne;
 
 public class Two {
     public static void main(String[] args) {
-        testIf();
+        testSwith();
     }
 
     // 條件混亂
@@ -16,6 +16,38 @@ public class Two {
             System.out.println("優秀"); //期望輸出
         }
     }
+
+    //缺少break,全部執行
+    public static void testSwith(){
+        int a = 1;
+        switch (a){ //輸出123
+            case 1:
+                System.out.println("1");
+            case 2:
+                System.out.println("2");
+            default:
+                System.out.println("3");
+        }
+    }
+
+    //case後可為空，執行第一個不為空語句
+    public static void testSwitchTwo(){
+        char c = 'A';
+        switch (c){
+            case 'A':
+            case 'B':
+            case 'C':
+                System.out.println("A-Z");
+                break;
+            case 'D':
+                System.out.println("others");
+                break;
+        }
+    }
+
+
+
+
 
 
 }
