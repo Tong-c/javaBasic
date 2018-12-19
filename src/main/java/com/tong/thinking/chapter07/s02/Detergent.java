2ed6cc22f98f2968a7a8562abda127c70aacd7f4
tong <1972376180@qq.com>
2018-12-19 18:25:01 +0800

    1.java編程思想：第七章示例

---
+++ src/main/java/com/tong/thinking/chapter07/s02/Detergent.java
@@ -0,0 +1,26 @@
+package com.tong.thinking.chapter07.s02;
+
+public class Detergent extends Cleanser {
+    public void scrub() {
+        append(" Detergent.scrub()");
+        super.scrub();
+    }
+
+    public void foam() {
+        append(" foam() ");
+    }
+
+    public static void main(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Testing base class:");
+        Cleanser.main(args);
+    }
+
+
+
+}
